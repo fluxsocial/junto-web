03eaceca9f982f1b92d27b1a1e47349c691edd6c
Eric Yang <eric@junto.foundation>
2021-04-01 14:17:08 -0600

    Update project template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+$junto-primary-dark: #000;
+$junto-primary: #333;
+$junto-primary-medium: #555;
+$junto-primary-light: #999;
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
 }
 
-#nav {
-  padding: 30px;
+html {
+  font-size: 62.5%;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  font-family: "Avenir", "Raleway", sans-serif;
+  box-sizing: border-box;
+  font-weight: 400;
+  overflow-x: hidden;
+  outline: none;
+  color: $junto-primary;
 }
 </style>
