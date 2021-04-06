d085fca1378a86a623dbbe366a0b4ad89dc2c2f7
Eric Yang <eric@junto.foundation>
2021-04-05 22:56:45 -0500

    Create toggle theme button

---
+++ src/components/ui/theme/ToggleThemeButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <button class="toggleThemeButton" @click="toggleTheme">
+    <img class="toggleThemeButton__icon" :src="themeIcon" alt="" />
+  </button>
+</template>
+
+
+<script>
+export default {
+  methods: {
+    toggleTheme() {
+      if (this.currentTheme === "light") {
+        this.$store.commit({ type: "toggleTheme", value: "dark" });
+      } else {
+        this.$store.commit({ type: "toggleTheme", value: "light" });
+      }
+    },
+  },
+  computed: {
+    currentTheme() {
+      return this.$store.getters.getCurrentTheme;
+    },
+    themeIcon() {
+      if (this.currentTheme === "light") {
+        return require("../../../assets/icons/sun.png");
+      } else {
+        return require("../../../assets/icons/moon.png");
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.toggleThemeButton {
+  padding: 1rem 2rem;
+  outline: none;
+  border: 1px solid var(--junto-border-color);
+  color: var(--junto-background-color);
+  margin-bottom: 2rem;
+  border-radius: 15px;
+
+  &:hover {
+    cursor: pointer;
+  }
+  &__icon {
+    height: 1.7rem;
+  }
+}
+</style>
