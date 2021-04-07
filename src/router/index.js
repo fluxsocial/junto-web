22605e2e95ad24957537448c33ada6a7d1e43da7
Eric Yang <eric@junto.foundation>
2021-04-06 23:22:29 -0500

    update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import AppView from './../views/app-view/AppView.vue';
+import AppView from './../views/app-view/AppView.vue';
 import WelcomeView from './../views/welcome-view/WelcomeView.vue';
 
 const routes = [
   {
     name: '/',
     component: WelcomeView
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: AppView
   }
 
 ]
