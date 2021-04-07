d41a6fed5be05c741a9337c69f8fa10d33d34db4
Fayeed Pawaskar <fayeed@live.com>
2021-04-07 10:01:26 +0530

    feat(app) - Added shims to handle different file types

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,9 @@
+import { Store } from 'src/store';// path to store file
+
+
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
