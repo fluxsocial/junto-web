59577792f41f469133313cbdf43e0bcca7599088
Eric Yang <eric@junto.foundation>
2021-04-22 03:01:10 -0500

    Finish building demo 1

--- src/components/ui/avatar/CommunityAvatar.vue
+++ src/components/ui/avatar/CommunityAvatar.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="community-avatar" @click="navToCommunity()">
+  <div
+    class="community-avatar"
+    @click="navToCommunity()"
+    :style="avatarStyling"
+  >
     <div class="community-avatar__name">
       <p class="community-avatar__name--text">{{ community.name }}</p>
     </div>
@@ -13,11 +17,19 @@ import { useStore, Community } from '@/store';
 export default defineComponent({
   props: {
     community: Object as PropType<Community>,
+    profileImage: {
+      default: require('@/assets/images/junto_app_icon.png'),
+    },
   },
   methods: {
     navToCommunity() {
       const store = useStore();
       store.commit({ type: 'changeCommunity', value: this.community });
+    },
+  },
+  computed: {
+    avatarStyling(): string {
+      return `background-image: url('${this.profileImage}');`;
     },
   },
 });
@@ -31,7 +43,6 @@ export default defineComponent({
   background-color: var(--junto-border-color);
   border-radius: 100px;
   margin-bottom: 2rem;
-  background-image: url('../../../../src/assets/images/junto_app_icon.png');
   background-size: cover;
   position: relative;
 
